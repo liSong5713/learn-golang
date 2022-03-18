d06fa28721781c182cb93f72b6768cbd42b1e963
rick.li <rick.li@ly.com>
2022-03-18 10:48:32 +0800

    feat: 增加函数demo

--- cycle-statement/index.go
+++ cycle-statement/index.go
@@ -4,12 +4,16 @@ import "fmt"
 
 func cycel() {
 	var a int = 1
+	// way1
 	for a < 10 {
 		fmt.Printf("a的值为:%d\n", a)
 		if a == 5 {
 			break
 		}
 		a++
+	}
+	// way2
+	for b := 0; b < 10; b++ {
 	}
 }
 
